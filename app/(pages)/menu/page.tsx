abdb47e3c8cf509fd9e56d3429a1b66c0183f18b
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2026-01-06 10:40:33 +0700

    Update btn LogoutButton

--- app/(pages)/menu/page.tsx
+++ app/(pages)/menu/page.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
+import LogoutButton from "@/components/logout-button";
+
 export default async function MenuPage() {
     const session = await getServerSession(authOptions);
 
@@ -10,16 +12,23 @@ export default async function MenuPage() {
     }
 
     return (
-        <div className="min-h-screen bg-gray-900 text-white p-6">
-            <h1 className="text-3xl font-bold mb-4">
-                Welcome {session.user?.name}
-            </h1>
+        <div className="min-h-screen bg-gray-900 text-white p-10">
+            <div className="flex justify-between gap-4">
+                <h1 className="text-3xl font-bold mb-4">
+                    Welcome "{session.user?.name}"
+                </h1>
+                <div >
+                    <LogoutButton />
+                </div>
+            </div>
 
             <ul className="space-y-3">
                 <li className="bg-gray-800 p-4 rounded">📊 Dashboard</li>
                 <li className="bg-gray-800 p-4 rounded">📦 Products</li>
                 <li className="bg-gray-800 p-4 rounded">⚙️ Settings</li>
             </ul>
+
+
         </div>
     );
 }
